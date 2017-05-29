c227ec408efaf3a3f21eb995513e57a7576f1aee
Nurhanifahmawadah <hanifahmawadah@gmail.com>
2017-05-29 14:28:06 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,15 @@
+<html>
+<head><title>Belajar Function dalam PHP</title></head>
+<body bgcolor="#00FFFF">
+<?php
+function panggil() //nama fungsi
+{ echo"Merupakan function tanpa paramater<br>";
+echo"wah...mudah yah..?";
+}
+//echo"<u>Dipanggil pertama kali</u><br>";
+panggil();
+//echo"<br><br><u>Dipanggil untuk kedua kalinya</u><br>";
+//panggil();
+?>
+</body>
+</html>
